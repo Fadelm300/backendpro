78b00ecb425a2dbfb28acb76d287ef2b9af60b6a
Fadem300 <fadel.m200@gmail.com>
2025-10-28 17:14:36 +0300

    try nu 101

--- api/server.js
+++ api/server.js
@@ -4,25 +4,19 @@ import dotenv from "dotenv";
 import contactRouter from "../routes/contact.js";
 
 dotenv.config();
-
 const app = express();
 
 // Middleware
-
 app.use(cors({
   origin: (origin, callback) => {
-    if (!origin) return callback(null, true); // requests من Postman أو server-to-server
-    if (origin.startsWith("http://localhost")) {
-      return callback(null, true); // السماح لأي بورت محلي
-    }
-    if (origin === "https://fadelprofile.vercel.app") {
-      return callback(null, true); // السماح للفرونت على Vercel
-    }
+    if (!origin) return callback(null, true); // Postman أو server-to-server
+    if (origin.startsWith("http://localhost")) return callback(null, true);
+    if (origin === "https://fadelprofile.vercel.app") return callback(null, true);
     return callback(new Error("Not allowed by CORS"));
   },
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
-  optionsSuccessStatus: 200 // مهم للـ preflight
+  optionsSuccessStatus: 204 // مهم للـ preflight request
 }));
 
 app.use(express.json());
